2f39b314084f494f4f196a1637fe34b453bc92b6
potato520 <alexa456@163.com>
2016-07-18 02:46:50 +0800

    对TP框架的Common下的function.php添加sendMail()邮件函数

--- Common/Common/function.php
+++ Common/Common/function.php
@@ -2,8 +2,8 @@
 /**
  * @Author: y
  * @Date:   2016-06-29 17:00:59
- * @Last Modified by:   y
- * @Last Modified time: 2016-06-29 22:32:17
+ * @Last Modified by:   potato520
+ * @Last Modified time: 2016-07-18 02:41:26
  */
 
 // 自动加载的函数
@@ -50,6 +50,48 @@ function request($url, $https=true, $method="get", $data=null){
 }     
 
   
+
+#封装邮件操作方法
+/**
+*@param $subject 信件标题
+*@param $msghtml 信件内容
+*@param $sendAddress 收件人邮箱
+**/
+function sendMail($subject,$msghtml,$sendAddress){
+  //因为如果发送到的地址已经为空了，我们就没必要去加载类文件
+  if($sendAddress == ''){
+    //邮件为空，返回false;
+    return false;
+  }
+  //引入发送类phpmailer.php
+  include './Component/class.phpmailer.php';
+  //实列化对象
+  $mail             = new PHPMailer();
+  /*服务器相关信息*/
+  $mail->IsSMTP();                        //设置使用SMTP服务器发送
+  $mail->SMTPAuth   = true;               //开启SMTP认证
+  $mail->Host       = 'smtp.163.com';         //设置 SMTP 服务器,自己注册邮箱服务器地址
+  $mail->Username   = 'woai281';      //发信人的邮箱用户名
+  $mail->Password   = 'itcastphp123';          //发信人的邮箱密码
+  /*内容信息*/
+  $mail->IsHTML(true);               //指定邮件内容格式为：html
+  $mail->CharSet    ="UTF-8";          //编码
+  $mail->From       = 'woai281@163.com';       //发件人完整的邮箱名称
+  $mail->FromName   ="php47的商城";      //发信人署名
+  $mail->Subject    = $subject;         //信的标题
+  $mail->MsgHTML($msghtml);           //发信主体内容
+  // $mail->AddAttachment("fish.jpg");      //附件
+  /*发送邮件*/
+  $mail->AddAddress($sendAddress);        //收件人地址
+  //使用send函数进行发送
+  if($mail->Send()) {
+      //发送成功返回真
+      return true;
+     // echo '您的邮件已经发送成功！！！';
+  } else {
+     return  $mail->ErrorInfo;//如果发送失败，则返回错误提示
+  }
+}     
 
 
 
